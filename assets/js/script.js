5184619b0ec9597cfabf497b1d82e0ec5757b124
marked-gil <markgil_creates@yahoo.com>
2022-06-15 16:42:43 +0000

    Add lists of questions

---
+++ assets/js/script.js
@@ -0,0 +1,186 @@
+// EASY QUESTIONS
+let easyQuestions = [
+    {
+        question: "What is the capital of France?",
+        correctAnswer: "Paris",
+        wrongAnswers: ["Italy", "Cannes", "Marseille"]
+	},
+    {
+        question: "Which Queen of Pop was married to Guy Ritchie and Sean Penn?",
+        correctAnswer: "Madonna",
+        wrongAnswers: ["Celine Dion", "Mariah Carey", "Cher"]
+	},
+    {
+        question: "What brand is also known as the Golden Arches?",
+        correctAnswer: "McDonalds",
+        wrongAnswers: ["KFC", "Kenny Rogers", "Subway"]
+	},
+    {
+        question: "Often described as the longest river on earth, on which continent would you find the Nile?",
+        correctAnswer: "Africa",
+        wrongAnswers: ["Asia", "Europe", "North America"]
+	},
+    {
+        question: "What elements make up water?",
+        correctAnswer: "oxygen and hydrogen",
+        wrongAnswers: ["oxygen and nitrogen", "oxygen and carbon", "oxygen and sodium"]
+	},
+    {
+        question: "Which scientist came up with the theory of E=MC2?",
+        correctAnswer: "Albert Einstein",
+        wrongAnswers: ["Isaac Newton", "Marie Curie", "Stephen Hawking"]
+	},
+    {
+        question: "What animal is Pumbaa in the Lion King?",
+        correctAnswer: "warthog",
+        wrongAnswers: ["tiger", "meerkat", "mandrill"]
+	},
+    {
+        question: "How many cards are there in a pack of cards (excluding jokers)?",
+        correctAnswer: 52,
+        wrongAnswers: [32, 60, 50]
+	},
+    {
+        question: "What bird is famous for being pink?",
+        correctAnswer: "Flamingo",
+        wrongAnswers: ["turaco", "southern cassowary", "bar-throated minla"]
+	},
+    {
+        question: "What planet is closest to the sun? ",
+        correctAnswer: "Mercury",
+        wrongAnswers: ["Saturn", "Venus", "Pluto"]
+	},
+]
+
+// MODERATE QUESTIONS
+let moderateQuestions = [
+    {
+        question: "Which British King famously had six wives?",
+        correctAnswer: "Henry VIII",
+        wrongAnswers: ["George VI", "George V", "William IV"]
+	},
+    {
+        question: "Conquering much of China and central Asia, uniting the wandering tribes and even responsible for creating one of the first international postal systems, who led the Mongols?",
+        correctAnswer: "Genghis Khan",
+        wrongAnswers: ["Kublai Khan", "The Bogd Khan", "Kaidu" ]
+	},
+    {
+        question: "Which German classical composer and pianist, was the film Immortal Beloved about?",
+        correctAnswer: "Ludwig van Beethoven",
+        wrongAnswers: ["Mozart", "Johannes Brahms", "Felix Mendelssohn"]
+	},
+    {
+        question: "In which country would you find Angel Falls, the highest waterfall on earth?",
+        correctAnswer: "Venezuela",
+        wrongAnswers: ["Mexico", "Canada", "Brazil"]
+	},
+    {
+        question: "The gaseous elements helium, neon, argon, krypton, xenon, and radon are better known as what?",
+        correctAnswer: "noble gases",
+        wrongAnswers: ["halogens", "simple gases", "liquified gases"]
+	},
+    {
+        question: "What is the name of Saturn’s largest moon?",
+        correctAnswer: "Titan",
+        wrongAnswers: ["Enceladus", "Mimas", "Rhea"]
+	},
+    {
+        question: "What character has both Robert Downey Jr. and Benedict Cumberbatch played?",
+        correctAnswer: "Sherlock Holmes",
+        wrongAnswers: ["'Mad' Max Rockatansky", "Charles Spencer Chaplin", "Alan Turing"]
+	},
+    {
+        question: "What country drinks the most coffee per person?",
+        correctAnswer: "Finland",
+        wrongAnswers: ["USA", "Canada", "Denmark"]
+	},
+    {
+        question: "What company was initially known as 'Blue Ribbon Sports'?",
+        correctAnswer: "Nike",
+        wrongAnswers: ["Addidas", "Puma", "Reebok"]
+	},
+    {
+        question: "What are the colours of the Olympic Rings?",
+        correctAnswer: "red, green, black, blue and yellow",
+        wrongAnswers: ["green, yellow, violet, blue, white", "yellow, red, brown, blue, green", "black, orange, yellow, green, red"]
+	},
+]
+
+// HARD QUESTIONS
+let hardQuestions = [
+    {
+        question: "Aureolin is a shade of what colour?",
+        correctAnswer: "yellow",
+        wrongAnswers: ["pink", "orange", "blue"]
+	},
+    {
+        question: "In what country is the Chernobyl nuclear plant located?",
+        correctAnswer: "Ukraine",
+        wrongAnswers: ["Austria", "Norway", "Poland"]
+	},
+    {
+        question: "How many dots appear on a pair of dice?",
+        correctAnswer: 42,
+        wrongAnswers: [21, 52, 32]
+	},
+    {
+        question: "What country has the only national flag that does not have four sides?",
+        correctAnswer: "Nepal",
+        wrongAnswers: ["Luxembourg", "Taiwan", "Tuvalu"]
+	},
+    {
+        question: "What sporting event has a strict dress code of almost entirely white?",
+        correctAnswer: "Wimbledon",
+        wrongAnswers: ["Tour de France", "Royal Ascot", "US Masters Tournament"]
+	},
+    {
+        question: "How many minutes are in a full week?",
+        correctAnswer: "10,080 minutes",
+        wrongAnswers: ["20,160 minutes", "43,800 minutes", "7,200 minutes"]
+	},
+    {
+        question: "In what capital would you find The Little Mermaid statue?",
+        correctAnswer: "Copenhagen",
+        wrongAnswers: ["Vienna", "Helsinki", "Athens"]
+	},
+    {
+        question: "What is Cynophobia the fear of?",
+        correctAnswer: "dogs",
+        wrongAnswers: ["cats", "computers", "accidents"]
+	},
+    {
+        question: "Unix Time is defined as the number of seconds that have elapsed since when?",
+        correctAnswer: "midnight of January 1, 1970",
+        wrongAnswers: ["midnight of January 1, 1987", "noontime of January 1, 1970", "noontime of January 1, 1987"]
+	},
+    {
+        question: "Who first coined the term 'battery' to describe an electrical storage device?",
+        correctAnswer: "Benjamin Franklin",
+        wrongAnswers: ["Henry Ford", "Michael Faraday", "Albert Einstein"]
+	},
+]
+
+
+// MILLION EURO QUESTIONS
+let hardestQuestions = [
+    {
+        question: "What is a group of pandas known as?",
+        correctAnswer: "an embarassment",
+        wrongAnswers: ["a shrewdness", "a murder", "a convocation"]
+	},
+    {
+        question: "How long is an elephant pregnant before it gives birth?",
+        correctAnswer: "22 months",
+        wrongAnswers: ["9 months", "12 months", "16 months"]
+	},
+    {
+        question: "The first successful vaccine was introduced by Edward Jenner in 1796. Which disease did it guard against?",
+        correctAnswer: "Smallpox",
+        wrongAnswers: ["Yellow Fever", "Poliomyelitis", "Tuberculosis"]
+	},
+    {
+        question: "Who is the only musician ever to have been awarded the Nobel prize for literature?",
+        correctAnswer: "Bob Dylan",
+        wrongAnswers: ["Jimi Hendrix", "Elton John", "Stevie Wonder"]
+	},
+]
