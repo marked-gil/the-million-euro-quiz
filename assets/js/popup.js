cec8cecf3f5ac64244d51bbabe976c62bd80eb64
marked-gil <markgil_creates@yahoo.com>
2022-06-22 18:13:39 +0000

    Add ContentLoaded event listener

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,11 +1,13 @@
-let howToPlayArticle = document.getElementById('how-to-play-container');
-let howToPlayBtn = document.getElementById('how-to-play-button');
+document.addEventListener('DOMContentLoaded', function(event) {
+    let howToPlayArticle = document.getElementById('how-to-play-container');
+    let howToPlayBtn = document.getElementById('how-to-play-button');
 
-howToPlayBtn.addEventListener('click', function() {
-    howToPlayArticle.classList.remove('hide');
-})
+    howToPlayBtn.addEventListener('click', function() {
+        howToPlayArticle.classList.remove('hide');
+    })
 
-let closeHowToPlay = document.getElementById('close-how-to-play');
-closeHowToPlay.addEventListener('click', function() {
-    howToPlayArticle.classList.add('hide');
+    let closeHowToPlay = document.getElementById('close-how-to-play');
+    closeHowToPlay.addEventListener('click', function() {
+        howToPlayArticle.classList.add('hide');
+    })
 })
