cd2ff63948dcfadf8ef7a1134707a46cbf786ced
marked-gil <markgil_creates@yahoo.com>
2022-06-22 17:02:18 +0000

    Create new javascript for popup contents

---
+++ assets/js/popup.js
@@ -0,0 +1,6 @@
+let howToPlayArticle = document.getElementById('how-to-play-container');
+let howToPlayBtn = document.getElementById('how-to-play-button');
+
+howToPlayBtn.addEventListener('click', function() {
+    howToPlayArticle.classList.remove('hide');
+})
