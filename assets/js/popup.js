a888f32d7a8346daa6eee6462564e7a02eae4beb
marked-gil <markgil_creates@yahoo.com>
2022-06-22 17:06:09 +0000

    Make 'x' icon work in closing 'how to play' popup section

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -4,3 +4,8 @@ let howToPlayBtn = document.getElementById('how-to-play-button');
 howToPlayBtn.addEventListener('click', function() {
     howToPlayArticle.classList.remove('hide');
 })
+
+let closeHowToPlay = document.getElementById('close-how-to-play');
+closeHowToPlay.addEventListener('click', function() {
+    howToPlayArticle.classList.add('hide');
+})
