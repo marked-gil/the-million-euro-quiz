ff217d0caae085eb34aa1c76899bbee19a63e411
marked-gil <markgil_creates@yahoo.com>
2022-06-22 08:42:21 +0000

    Restructure comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -246,17 +246,23 @@ document.addEventListener('DOMContentLoaded', function(event) {
         lifeItem.addEventListener('click', function() {
             if (this.getAttribute('data-lifeline') === 'remove-one-option') {
                 randomChoiceRemove(selectedQuestion, 1, answerButtons);
-                // deactives the lifeline button after using. This code is from stackoverflow: https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript
+                /* deactives the lifeline button after using. 
+                This code is from stackoverflow: 
+                https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript */
                 this.removeEventListener('click', arguments.callee);
             } else if (this.getAttribute('data-lifeline') === 'remove-two-options') {
                 randomChoiceRemove(selectedQuestion, 2, answerButtons);
-                // deactives the lifeline button after using. This code is from stackoverflow: https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript
+                /* deactives the lifeline button after using. 
+                This code is from stackoverflow: 
+                https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript */
                 this.removeEventListener('click', arguments.callee);
             } else {
                 // stops and adds more time to current timer, and then resumes new timer
                 clearInterval(counter.id);
                 counter.id = addMoreTime(timerValue, 30);
-                // deactives the lifeline button after using. This code is from stackoverflow: https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript
+                /* deactives the lifeline button after using. 
+                This code is from stackoverflow: 
+                https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript */
                 this.removeEventListener('click', arguments.callee);
             }
     });
