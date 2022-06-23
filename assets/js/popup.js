613441bca368ae4888287b9dc4b460f5bdacc0c4
marked-gil <markgil_creates@yahoo.com>
2022-06-23 10:01:57 +0000

    Change id of 'how to play' container in homepage

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-    let howToPlayArticle = document.getElementById('how-to-play-container');
+    let howToPlayArticle = document.getElementById('howtoplay-container');
     let howToPlayBtn = document.getElementById('howtoplay-btn-homepage');
 
     howToPlayBtn.addEventListener('click', function() {
