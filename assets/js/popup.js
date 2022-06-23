525c5018304fae2b14e3e9614edb0144a4bd93ff
marked-gil <markgil_creates@yahoo.com>
2022-06-23 09:58:08 +0000

    Change id for 'how to play' button on homepage

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(event) {
     let howToPlayArticle = document.getElementById('how-to-play-container');
-    let howToPlayBtn = document.getElementById('how-to-play-button');
+    let howToPlayBtn = document.getElementById('howtoplay-btn-homepage');
 
     howToPlayBtn.addEventListener('click', function() {
         howToPlayArticle.classList.remove('hide');
