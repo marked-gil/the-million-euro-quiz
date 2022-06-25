bdc65ed4b6eed9776295d34990f8372106d782c3
marked-gil <markgil_creates@yahoo.com>
2022-06-25 08:57:31 +0000

    Fix Uncaught TypeError: Cannot set properties of null

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,7 +74,7 @@ let moderateQuestions = [
         id: 12,
         question: "Conquering much of China and central Asia, uniting the wandering tribes and even responsible for creating one of the first international postal systems, who led the Mongols?",
         correctAnswer: "Genghis Khan",
-        wrongAnswers: ["Kublai Khan", "The Bogd Khan", "Kaidu" ]
+        wrongAnswers: ["Kublai Khan", "The Bogd Khan", "Kaidu"]
 	},
     {
         id: 13,
@@ -218,7 +218,8 @@ let hardestQuestions = [
 	},
 ];
 
-document.addEventListener('DOMContentLoaded', function(event) {
+// START --> HOME PAGE
+if (document.getElementById('homepage-body')) {
     let howToPlayArticle = document.getElementById('howtoplay-container');
     let howToPlayBtn = document.getElementById('howtoplay-btn-homepage');
 
@@ -230,99 +231,103 @@ document.addEventListener('DOMContentLoaded', function(event) {
     closeHowToPlay.addEventListener('click', function() {
         howToPlayArticle.classList.add('hide');
     })
-})
+}
+// END --> HOME PAGE
 
-
-document.addEventListener('DOMContentLoaded', function(event) {
-    // local variables for text contents of each question
-    let questionText = document.getElementById('question-text');
-    // list of button elements containing the answer options
-    let optionButtonsList = document.getElementsByClassName('answer-button');
-
-    let questionNumber = document.getElementById('question-number');
-
-    // list container for the IDs of used questions
-    let usedQuestions = [];
-
-    // selects and displays first question
-    let selectedQuestion = questionRandomizer(easyQuestions);
-    displayQuestion(selectedQuestion, questionNumber, questionText, optionButtonsList);
-
-    // timer
-    let timerValue = document.getElementById('timer-value');
-    let counter = timer(timerValue, 30);
-
-    // lifelines event listeners
-    let lifeLines = document.getElementsByClassName('life-line-item');
-    for (let lifeItem of lifeLines) {
-        lifeItem.addEventListener('click', function() {
-            if (this.getAttribute('data-lifeline') === 'remove-one-option') {
-                randomChoiceRemove(selectedQuestion, 1, optionButtonsList);
-                // applies 'disabled button' css style once the lifeline button is clicked
-                this.classList.add("disabled-button");
-                /* deactives the lifeline button after using. 
-                This code is from stackoverflow: https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript */
-                this.removeEventListener('click', arguments.callee);
-            } else if (this.getAttribute('data-lifeline') === 'remove-two-options') {
-                randomChoiceRemove(selectedQuestion, 2, optionButtonsList);
-                // applies 'disabled button' css style once the lifeline button is clicked
-                this.classList.add("disabled-button");
-                /* deactives the lifeline button after using. 
-                This code is from stackoverflow: https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript */
-                this.removeEventListener('click', arguments.callee);
-            } else {
-                // stops and adds more time to current timer, and then resumes new timer
-                clearInterval(counter.id);
-                counter.id = addMoreTime(timerValue, 30);
-                // applies 'disabled button' css style once the lifeline button is clicked
-                this.classList.add("disabled-button");
-                /* deactives the lifeline button after using. 
-                This code is from stackoverflow: 
-                https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript */
-                this.removeEventListener('click', arguments.callee);
-            }
-    });
-    }
-
-    // buttons for answer choices
-    for (let answerBtn of optionButtonsList) {
-        answerBtn.addEventListener('click', function() {
-            clearInterval(counter.id);
-            if (answerChecker(selectedQuestion, this.innerText.slice(3))) {
-                undisableOptionBtns(optionButtonsList);
-                usedQuestions.push(selectedQuestion.id);
-                counter = timer(timerValue, 30);
-                if (usedQuestions.length < 15) {
-                    alert("Continue game");
-                    if (usedQuestions.length <= 4){
-                        selectedQuestion = questionRandomizer(easyQuestions);
-                        while (usedQuestions.includes(selectedQuestion.id)) {
-                            selectedQuestion = questionRandomizer(easyQuestions);
-                        }
-                    } else if (usedQuestions.length > 4 && usedQuestions.length < 10) {
-                        selectedQuestion = questionRandomizer(moderateQuestions);
-                        while (usedQuestions.includes(selectedQuestion.id)) {
-                            selectedQuestion = questionRandomizer(moderateQuestions);
-                        }
-                    } else if (usedQuestions.length >= 10 && usedQuestions.length < 14) {
-                        selectedQuestion = questionRandomizer(hardQuestions);
-                        while (usedQuestions.includes(selectedQuestion.id)) {
-                            selectedQuestion = questionRandomizer(hardQuestions);
-                        }
-                    } else {
-                        selectedQuestion = questionRandomizer(hardestQuestions);
-                    }
-                    displayQuestion(selectedQuestion, questionNumber, questionText, optionButtonsList);
+// START -- > GAME PAGE
+if (document.getElementById('gamepage-body')) {
+    document.addEventListener('DOMContentLoaded', function(event) {
+        // local variables for text contents of each question
+        let questionText = document.getElementById('question-text');
+        // list of button elements containing the answer options
+        let optionButtonsList = document.getElementsByClassName('answer-button');
+    
+        let questionNumber = document.getElementById('question-number');
+    
+        // list container for the IDs of used questions
+        let usedQuestions = [];
+    
+        // selects and displays first question
+        let selectedQuestion = questionRandomizer(easyQuestions);
+        displayQuestion(selectedQuestion, questionNumber, questionText, optionButtonsList);
+    
+        // timer
+        let timerValue = document.getElementById('timer-value');
+        let counter = timer(timerValue, 30);
+    
+        // lifelines event listeners
+        let lifeLines = document.getElementsByClassName('life-line-item');
+        for (let lifeItem of lifeLines) {
+            lifeItem.addEventListener('click', function() {
+                if (this.getAttribute('data-lifeline') === 'remove-one-option') {
+                    randomChoiceRemove(selectedQuestion, 1, optionButtonsList);
+                    // applies 'disabled button' css style once the lifeline button is clicked
+                    this.classList.add("disabled-button");
+                    /* deactives the lifeline button after using. 
+                    This code is from stackoverflow: https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript */
+                    this.removeEventListener('click', arguments.callee);
+                } else if (this.getAttribute('data-lifeline') === 'remove-two-options') {
+                    randomChoiceRemove(selectedQuestion, 2, optionButtonsList);
+                    // applies 'disabled button' css style once the lifeline button is clicked
+                    this.classList.add("disabled-button");
+                    /* deactives the lifeline button after using. 
+                    This code is from stackoverflow: https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript */
+                    this.removeEventListener('click', arguments.callee);
                 } else {
-                    alert('YOU WON!');
+                    // stops and adds more time to current timer, and then resumes new timer
+                    clearInterval(counter.id);
+                    counter.id = addMoreTime(timerValue, 30);
+                    // applies 'disabled button' css style once the lifeline button is clicked
+                    this.classList.add("disabled-button");
+                    /* deactives the lifeline button after using. 
+                    This code is from stackoverflow: 
+                    https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript */
+                    this.removeEventListener('click', arguments.callee);
                 }
-            } else {
-                gameOver();
-            }
-            console.log(usedQuestions);
         });
-    }
-});
+        }
+    
+        // buttons for answer choices
+        for (let answerBtn of optionButtonsList) {
+            answerBtn.addEventListener('click', function() {
+                clearInterval(counter.id);
+                if (answerChecker(selectedQuestion, this.innerText.slice(3))) {
+                    undisableOptionBtns(optionButtonsList);
+                    usedQuestions.push(selectedQuestion.id);
+                    counter = timer(timerValue, 30);
+                    if (usedQuestions.length < 15) {
+                        alert("Continue game");
+                        if (usedQuestions.length <= 4){
+                            selectedQuestion = questionRandomizer(easyQuestions);
+                            while (usedQuestions.includes(selectedQuestion.id)) {
+                                selectedQuestion = questionRandomizer(easyQuestions);
+                            }
+                        } else if (usedQuestions.length > 4 && usedQuestions.length < 10) {
+                            selectedQuestion = questionRandomizer(moderateQuestions);
+                            while (usedQuestions.includes(selectedQuestion.id)) {
+                                selectedQuestion = questionRandomizer(moderateQuestions);
+                            }
+                        } else if (usedQuestions.length >= 10 && usedQuestions.length < 14) {
+                            selectedQuestion = questionRandomizer(hardQuestions);
+                            while (usedQuestions.includes(selectedQuestion.id)) {
+                                selectedQuestion = questionRandomizer(hardQuestions);
+                            }
+                        } else {
+                            selectedQuestion = questionRandomizer(hardestQuestions);
+                        }
+                        displayQuestion(selectedQuestion, questionNumber, questionText, optionButtonsList);
+                    } else {
+                        alert('YOU WON!');
+                    }
+                } else {
+                    gameOver();
+                }
+                console.log('Used Questions:', usedQuestions);
+            });
+        }
+    });
+}
+// END -- > GAME PAGE
 
 /**
  * Returns randomized question from a category as specified
