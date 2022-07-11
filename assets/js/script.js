4852f13b43d79dd42472c45d5c844102a7b12b5b
marked-gil <markgil_creates@yahoo.com>
2022-07-11 19:28:29 +0000

    Add aria-label to buttons to solve accesibility issues in lightbouse

--- assets/js/script.js
+++ assets/js/script.js
@@ -646,6 +646,8 @@ function displayQuestion() {
     let letterSequence = ['A.', 'B.', 'C.', 'D.'];
     for (let i = 0; i < optionButtonsList.length; i++) {
         optionButtonsList[i].innerText = `${letterSequence[i]} ${shuffledOptions[i]}`;
+        optionButtonsList[i].setAttribute('aria-label', optionButtonsList[i].innerText);
+        console.log(optionButtonsList[i]);
         // un-disable buttons
         optionButtonsList[i].classList.remove('disabled-button');
     }
